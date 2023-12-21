a72cde3ca9a877532c4a5e9a3e2bc444fad9b25f
Uwe Naumann <uwe.naumann@rwth-aachen.de>
2023-12-21 09:03:20 +0100

    ...

--- code/Hessian.cpp
+++ code/Hessian.cpp
@@ -7,29 +7,44 @@ int main() {
   using namespace std;
   using namespace std::chrono;
   // increase to compare run times of various approaches
-  const int N=240101;
+  const int N=240101; // e.g., YYMMDD
   // code to be executed in integer arithmetic
   using T=char;
   // size of x yields squared number of Hessian entries
-  T x[N];
-  // vanishing inputs up to entry N-10
-  for (int i=0;i<N-10;++i) x[i]=0;
-  // custom input values x[N-10] ... x[N-1]
+   T x[N];
+  // pairs of custom input values distributed uniformly
   T xv[]={72,97,112,112,121,32,50,48,50,52};
-  for (int i=N-10;i<N;++i) x[i]=xv[i-(N-10)];
+  for (int i=0,j=0;i<N;++i) {
+    if ((!(i%(N/5)))&&(j<10)) {
+      x[i]=xv[j++];
+      x[i+++1]=xv[j++];
+    } else {
+      x[i]=0;
+    }
+  }
+
   // Season's Greetings ...
-  cout << "AD wishes ";
+  cout << "Second-Order Tangent AD wishes ";
   // start time measurement
-  auto begin=system_clock::now();
-  // compute diagonal of the Hessian in tangent ...
+  auto t_begin=system_clock::now();
+  // compute diagonal of the Hessian in tangent of tangent mode
   ddfdxx_t1_t2_sparse<N>(x);
-  // ... or adjoint mode 
-  // ddfdxx_a1_t2_compressed<N>(x);
   // finish time measurement
-  auto end=system_clock::now();
+  auto t_end=system_clock::now();
   // report run time 
   cout << "! (taking " 
-    << duration_cast<seconds>(end-begin).count()
-    << "s)" << endl;
+    << duration_cast<milliseconds>(t_end-t_begin).count()
+    << "ms)" << endl;
+  cout << "Second-Order Adjoint AD wishes ";
+  // start time measurement
+  t_begin=system_clock::now();
+  // compute diagonal of the Hessian in tangent of adjoint mode
+  ddfdxx_a1_t2_compressed<N>(x);
+  // finish time measurement
+  t_end=system_clock::now();
+  // report run time 
+  cout << "! (taking " 
+    << duration_cast<milliseconds>(t_end-t_begin).count()
+    << "ms)" << endl;
   return 0;
 }
--- code/gradient.cpp
+++ code/gradient.cpp
@@ -7,29 +7,44 @@ int main() {
   using namespace std;
   using namespace std::chrono;
   // increase to experience superior run time of adjoint
-  const int N=231224; // required to be even
+  const int N=231224; // e.g., YYMMDD; required to be even
   // code to be executed in integer arithmetic
   using T=char;
   // size of x equal to size of gradient
   T x[N];
-  // vanishing inputs up to entry N-10
-  for (int i=0;i<N-10;++i) x[i]=0;
-  // custom input values x[N-10] ... x[N-1]
+  // pairs of custom input values distributed uniformly
   T xv[]={101,77,114,114,32,121,109,88,115,97};
-  for (int i=N-10;i<N;++i) x[i]=xv[i-(N-10)];
-  // Season's Greetings ...
-  cout << "AD wishes ";
+  for (int i=0,j=0;i<N;++i) {
+    if ((!(i%(N/5)))&&(j<10)) { 
+      x[i]=xv[j++];
+      x[i+++1]=xv[j++];
+    } else {
+      x[i]=0;
+    }
+  }
+  // Season's Greetings by Tangent AD ...
+  cout << "Tangent AD wishes ";
   // start time measurement
-  auto begin=system_clock::now();
-  // compute gradient in tangent ...
+  auto t_begin=system_clock::now();
+  // compute gradient in tangent mode
   dfdx_t1<N>(x);
-  // ... or adjoint mode
-  // dfdx_a1<N>(x);
   // finish time measurement
-  auto end=system_clock::now();
+  auto t_end=system_clock::now();
   // report run time
   cout << "! (taking "
-    << duration_cast<seconds>(end-begin).count()
-    << "s)" << endl;
+    << duration_cast<milliseconds>(t_end-t_begin).count()
+    << "ms)" << endl;
+  // Season's Greetings by Tangent AD ...
+  cout << "Adjoint AD wishes ";
+  // start time measurement
+  t_begin=system_clock::now();
+  // compute gradient in adjoint mode
+  dfdx_a1<N>(x);
+  // finish time measurement
+  t_end=system_clock::now();
+  // report run time
+  cout << "! (taking "
+    << duration_cast<milliseconds>(t_end-t_begin).count()
+    << "ms)" << endl;
   return 0;
 }
